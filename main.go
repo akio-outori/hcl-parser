33708f056552be4b3c93e2f5d9ccb1ecf6f4a145
Jeff Hallyburton <jhally@gmail.com>
2020-08-14 11:02:23 -0400

    generalize

--- main.go
+++ main.go
@@ -1,47 +1,67 @@
 package main
 
 import (
+  "os"
   "fmt"
   "flag"
   "io/ioutil"
   "github.com/ghodss/yaml"
 )
 
+var input string
+var output string
+
+// It works every time
+func getHelp() {
+  fmt.Print("Usage:\n\n")
+  flag.PrintDefaults()
+  os.Exit(1)
+}
+
 func getYAML(input string) []byte {
   yml, err := ioutil.ReadFile(input)
-  check(err)
+  check_error(err)
   return yml
 }
 
 func convertYAML(input []byte) []byte {
   json, err := yaml.YAMLToJSON(input)
-  check(err)
+  check_error(err)
   return json
 }
 
 func writeJSON(input []byte, output string) {
-  check(ioutil.WriteFile(output, input, 0644))
+  check_error(ioutil.WriteFile(output, input, 0644))
 }
 
-func check(e error) {
+func check_error(e error) {
   if e != nil {
     panic(e)
   }
 }
 
-func main() {
+func check_variable(variable string) {
+  if variable == "" {
+    getHelp()
+  }
+}
 
-  var input string
-  var output string
-
+func init() {
+  // Set Variables
   flag.StringVar(&input, "input", "", "file to convert to json")
   flag.StringVar(&output, "output", "", "output file for the json rendered template")
+
+  // Parse flags here so that we can check them
   flag.Parse()
 
+  // Check Variables
+  check_variable(input)
+  check_variable(output)
+}
+
+func main() {
   yml  := getYAML(input)
   json := convertYAML(yml)
-
   writeJSON(json, output)
   fmt.Print(string(json))
-
 }
